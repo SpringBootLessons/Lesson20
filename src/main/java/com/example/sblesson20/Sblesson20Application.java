53586a1d4581b7da068d2526c19fb8e5e8d65dc2
Dave Wolf <dave45678@gmail.com>
2018-07-16 17:47:57 -0400

    Initial commit

---
+++ src/main/java/com/example/sblesson20/Sblesson20Application.java
@@ -0,0 +1,12 @@
+package com.example.sblesson20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sblesson20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sblesson20Application.class, args);
+	}
+}
---
+++ src/main/java/com/example/sblesson20/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.sblesson20;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Long> {
+    User findByUsername(String username);
+}
